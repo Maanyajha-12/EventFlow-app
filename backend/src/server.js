aca145761f431b784492c25715e583fdcf25e5da
Maanyajha-12 <maanya_j@ch.iitr.ac.in>
2026-03-18 09:45:04 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/events", eventRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Backend running on http://localhost:${PORT}`));
 
 
